5ebde94fcd2b61219d64cb5b1ca15933072795a1
admin <admin@Win7_1TB>
2018-10-29 09:20:23 +0100

    Dos

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -11,11 +11,10 @@ package helloworld;
  */
 public class HelloWorld {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
         System.out.println("Hola mundo");
     }
     
